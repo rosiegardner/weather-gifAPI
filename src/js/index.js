2e4a4f247f7fc7b5fbd43f083a4a435dfc7a40c8
Rosie Gardner <rosiegardner78@gmail.com>
2022-03-22 11:28:22 -0700

    add service file for giphy and weather

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,54 @@
+// Import statements updated to reflect new paths and also import GiphyService.
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../css/styles.css';
+import './css/styles.css';
+import WeatherService from './services/weather-service.js';
+import GiphyService from './services/giphy-service.js';
 
-console.log($)
+
+// clearFields() updated to reflect changed HTML.
+function clearFields() {
+  $('#location').val("");
+  $('.show-errors').text("");
+}
+
+// The three remaining UI functions make our code more modular. We'll discuss them soon.
+
+function displayWeatherDescription(description) {
+  $('.weather-description').text(`The weather is ${description}!`);
+}
+
+function displayGif(response) {
+  const url = response.data[0].images.downsized.url;
+  $('.show-gif').html(`<img src='${url}'>`);
+}
+
+function displayErrors(error) {
+  $('.show-errors').text(`${error}`);
+}
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    let city = $('#location').val();
+    clearFields();
+    WeatherService.getWeather(city)
+      .then(function(weatherResponse) {
+        if (weatherResponse instanceof Error) {
+          throw Error(`OpenWeather API error: ${weatherResponse.message}`);
+        }
+        const weatherDescription = weatherResponse.weather[0].description;
+        displayWeatherDescription(weatherDescription);
+        return GiphyService.getGif(weatherDescription);
+      })
+      .then(function(giphyResponse) {
+        if (giphyResponse instanceof Error) {
+          throw Error(`Giphy API error: ${giphyResponse.message}`);
+        }
+        displayGif(giphyResponse);
+      })
+      .catch(function(error) {
+        displayErrors(error.message);
+      })
+  });
+});
